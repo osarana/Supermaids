812bd53bdbc32a705d0c9d7589868f4935c11bcc
Zubaer Rahman Chowdhury <zrchy16@gmail.com>
2023-12-03 20:10:44 -0500

    Refactored order of code
    Added database view print statements after each part a-c.

--- supermaids_sqlite.py
+++ supermaids_sqlite.py
@@ -1,9 +1,10 @@
 import sqlite3
-import pandas as pd
 
 # Connect to SQLite database (creates a new database if it doesn't exist)
 conn = sqlite3.connect('cleaning_service.db')
 cursor = conn.cursor()
+
+### Part (a) ###
 
 # Create Employee Table
 cursor.execute('''
@@ -17,20 +18,6 @@ cursor.execute('''
     )
 ''')
 
-# Insert tuples into the Employee table
-employee_data = [
-    (1, 'Oscar', 'Arana', '123 Main St', 100000, '(954)644-3169'),
-    (2, 'Zubaer', 'Chowdhury', '123 Amaro Dr', 75000, '(303)534-1234'),
-    (3, 'Justin', 'Prince', '500 Ponce De Leon Blvd', 75000, '(123)219-1234'),
-    (4, 'John', 'Doe', '1300 Memorial Drive', 50000, '(786)555-1234'), 
-    (5, 'Jane', 'Smith', '456 Elm St', 60000, '(587)555-5678')
-]
-
-cursor.executemany('''
-    INSERT INTO Employee (staffNo, staffFName, staffLName, staffAddress, staffSalary, staffTelNo)
-    VALUES (?, ?, ?, ?, ?, ?)
-''', employee_data)
-
 # Create Client Table
 cursor.execute('''
     CREATE TABLE Client (
@@ -42,21 +29,7 @@ cursor.execute('''
     )
 ''')
 
-# Insert tuples into the Client table
-client_data = [
-    (1, 'Carol', 'Clark', '333 Maple St', '(123)555-1111'),
-    (2, 'David', 'Lee', '777 Birch St', '(456)555-2222'),
-    (3, 'Frank', 'Garcia', '888 Walnut St', '(252)555-3333'),
-    (4, 'Grace', 'Martinez', '999 Cherry St', '(985)555-4444'),
-    (5, 'Henry', 'Nguyen', '222 Chestnut St', '(786)555-5555')
-]
-
-cursor.executemany('''
-    INSERT INTO Client (clientNo, clientFName, clientLName, clientAddress, clientTelNo)
-    VALUES (?, ?, ?, ?, ?)
-''', client_data)
-
-# Create Request Table (HAVE ZUBAER CHECK TO SEE IF WE NEED CONSTRAINT ON STARTTIME)
+# Create Request Table
 cursor.execute('''
     CREATE TABLE Request (
         requestID INTEGER PRIMARY KEY,
@@ -69,20 +42,6 @@ cursor.execute('''
     )
 ''')
 
-# Insert tuples into the Request table
-request_data = [
-    (1, '2023-12-15', '10:00:00', 2, 'Need cleaning service', 1),
-    (2, '2023-12-20', '15:30:00', 3, 'Deep cleaning required', 2),
-    (3, '2023-12-10', '12:45:00', 1, 'Regular cleaning', 3),
-    (4, '2023-12-09', '10:00:00', 4, 'Urgent cleaning needed', 4),
-    (5, '2023-12-18', '11:15:00', 2, 'Specialized cleaning requested', 5)
-]
-
-cursor.executemany('''
-    INSERT INTO Request (requestID, startDate, startTime, duration, comments, clientNo)
-    VALUES (?, ?, ?, ?, ?, ?)
-''', request_data)
-
 # Create Equipment Table
 cursor.execute('''
     CREATE TABLE Equipment (
@@ -92,20 +51,6 @@ cursor.execute('''
         cost INTEGER CHECK(cost >= 0)
     )
 ''')
-
-# Insert tuples into the Equipment table
-equipment_data = [
-    (1, 'Vacuum Cleaner', 'Household', 200),
-    (2, 'Mop and Bucket', 'Floor Cleaning', 50),
-    (3, 'Dustpan and Brush', 'Cleaning Tools', 20),
-    (4, 'Steam Cleaner', 'Deep Cleaning', 300),
-    (5, 'Scrubbing Brushes', 'Surface Cleaning', 30)
-]
-
-cursor.executemany('''
-    INSERT INTO Equipment (equipmentID, description, usage, cost)
-    VALUES (?, ?, ?, ?)
-''', equipment_data)
 
 # Create RequestEquipmentAssignment table
 cursor.execute('''
@@ -118,20 +63,6 @@ cursor.execute('''
     )
 ''')
 
-# Insert tuples into the RequestEquipmentAssignment table
-request_equipment_data = [
-    (1, 1, 1),
-    (2, 2, 2),
-    (3, 3, 1),
-    (4, 4, 3),
-    (5, 5, 2)
-]
-
-cursor.executemany('''
-    INSERT INTO RequestEquipmentAssignment (requestID, equipmentID, quantity)
-    VALUES (?, ?, ?)
-''', request_equipment_data)
-
 # Create EmployeeRequestAssignment table
 cursor.execute('''
     CREATE TABLE EmployeeRequestAssignment (
@@ -143,6 +74,106 @@ cursor.execute('''
     )
 ''')
 
+# Print each table after tables been created
+print("\n<<<========Tables Created========>>>")
+
+cursor.execute('SELECT * FROM Employee')
+print("\n==========Employee Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM Client')
+print("\n==========Client Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM Request')
+print("\n==========Request Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM Equipment')
+print("\n==========Equipment Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM RequestEquipmentAssignment')
+print("\n==========RequestEquipmentAssignment Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM EmployeeRequestAssignment')
+print("\n==========EmployeeRequestAssignment Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+### Part (b) ###
+
+# Insert tuples into the Employee table
+employee_data = [
+    (1, 'Oscar', 'Arana', '123 Main St', 100000, '(954)644-3169'),
+    (2, 'Zubaer', 'Chowdhury', '123 Amaro Dr', 75000, '(303)534-1234'),
+    (3, 'Justin', 'Prince', '500 Ponce De Leon Blvd', 75000, '(123)219-1234'),
+    (4, 'John', 'Doe', '1300 Memorial Drive', 50000, '(786)555-1234'), 
+    (5, 'Jane', 'Smith', '456 Elm St', 60000, '(587)555-5678')
+]
+cursor.executemany('''
+    INSERT INTO Employee (staffNo, staffFName, staffLName, staffAddress, staffSalary, staffTelNo)
+    VALUES (?, ?, ?, ?, ?, ?)
+''', employee_data)
+
+# Insert tuples into the Client table
+client_data = [
+    (1, 'Carol', 'Clark', '333 Maple St', '(123)555-1111'),
+    (2, 'David', 'Lee', '777 Birch St', '(456)555-2222'),
+    (3, 'Frank', 'Garcia', '888 Walnut St', '(252)555-3333'),
+    (4, 'Grace', 'Martinez', '999 Cherry St', '(985)555-4444'),
+    (5, 'Henry', 'Nguyen', '222 Chestnut St', '(786)555-5555')
+]
+cursor.executemany('''
+    INSERT INTO Client (clientNo, clientFName, clientLName, clientAddress, clientTelNo)
+    VALUES (?, ?, ?, ?, ?)
+''', client_data)
+
+# Insert tuples into the Request table
+request_data = [
+    (1, '2023-12-15', '10:00:00', 2, 'Need cleaning service', 1),
+    (2, '2023-12-20', '15:30:00', 3, 'Deep cleaning required', 2),
+    (3, '2023-12-10', '12:45:00', 1, 'Regular cleaning', 3),
+    (4, '2023-12-09', '10:00:00', 4, 'Urgent cleaning needed', 4),
+    (5, '2023-12-18', '11:15:00', 2, 'Specialized cleaning requested', 5)
+]
+cursor.executemany('''
+    INSERT INTO Request (requestID, startDate, startTime, duration, comments, clientNo)
+    VALUES (?, ?, ?, ?, ?, ?)
+''', request_data)
+
+# Insert tuples into the Equipment table
+equipment_data = [
+    (1, 'Vacuum Cleaner', 'Household', 200),
+    (2, 'Mop and Bucket', 'Floor Cleaning', 50),
+    (3, 'Dustpan and Brush', 'Cleaning Tools', 20),
+    (4, 'Steam Cleaner', 'Deep Cleaning', 300),
+    (5, 'Scrubbing Brushes', 'Surface Cleaning', 30)
+]
+cursor.executemany('''
+    INSERT INTO Equipment (equipmentID, description, usage, cost)
+    VALUES (?, ?, ?, ?)
+''', equipment_data)
+
+# Insert tuples into the RequestEquipmentAssignment table
+request_equipment_data = [
+    (1, 1, 1),
+    (2, 2, 2),
+    (3, 3, 1),
+    (4, 4, 3),
+    (5, 5, 2)
+]
+cursor.executemany('''
+    INSERT INTO RequestEquipmentAssignment (requestID, equipmentID, quantity)
+    VALUES (?, ?, ?)
+''', request_equipment_data)
+
 # Insert tuples into the EmployeeRequestAssignment table
 employee_request_data = [
     (1, 1, '2023-12-12'),
@@ -151,14 +182,48 @@ employee_request_data = [
     (4, 4, '2023-12-21'),
     (5, 5, '2023-12-20')
 ]
-
 cursor.executemany('''
     INSERT INTO EmployeeRequestAssignment (staffNo, requestID, assignmentDate)
     VALUES (?, ?, ?)
 ''', employee_request_data)
 
-# Queries
+# Print each table after tuples insertion
+print("\n<<<=======Tables After tuple Insertion=======>>>")
 
+cursor.execute('SELECT * FROM Employee')
+print("\n==========Employee Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM Client')
+print("\n==========Client Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM Request')
+print("\n==========Request Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM Equipment')
+print("\n==========Equipment Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM RequestEquipmentAssignment')
+print("\n==========RequestEquipmentAssignment Table==========")
+for row in cursor.fetchall():
+    print(row)
+
+cursor.execute('SELECT * FROM EmployeeRequestAssignment')
+print("\n==========EmployeeRequestAssignment Table==========")
+for row in cursor.fetchall():
+    print(row)
+print("\n")
+
+### Part (c) ###
+
+# Queries
 queries = [
     "SELECT staffFName, staffLName FROM Employee;",
     "SELECT clientFName, clientLName, clientAddress FROM Client;",
@@ -170,9 +235,8 @@ queries = [
     "FROM EmployeeRequestAssignment INNER JOIN Employee ON EmployeeRequestAssignment.staffNo = Employee.staffNo;"
 ]
 
-
-
 # Execute and print queries
+print("<<<========Queries========>>>")
 for index, query in enumerate(queries, 1):
     print(f"Query {index}: {query}") 
     cursor.execute(query)
